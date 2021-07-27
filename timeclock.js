1b637ffb3b07aa5b5bda399390a3ef570eeeaea2
immrakpk <35355294+immrakpk@users.noreply.github.com>
2021-07-26 23:41:51 -0400

    Add files via upload

---
+++ timeclock.js
@@ -0,0 +1,361 @@
+const img_list = [
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/1.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/2.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/3.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/4.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/5.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/6.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/7.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/8.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/9.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/10.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/11.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/12.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/13.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/14.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/15.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/16.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/17.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/18.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/19.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/20.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/21.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/22.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/23.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/24.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/25.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/26.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/27.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/28.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/29.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/30.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/31.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/32.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/33.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/34.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/35.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/36.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/37.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/38.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/39.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/40.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/41.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/42.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/43.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/44.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/45.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/46.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/47.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/48.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/49.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/50.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/51.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/52.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/53.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/54.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/55.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/56.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/57.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/58.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/59.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/60.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/61.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/62.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/63.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/64.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/65.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/66.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/67.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/68.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/69.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/70.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/71.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/72.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/73.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/74.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/75.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/76.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/77.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/78.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/79.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/80.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/81.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/82.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/83.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/84.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/85.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/86.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/87.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/88.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/89.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/90.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/91.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/92.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/93.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/94.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/95.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/96.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/97.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/98.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/99.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/100.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/101.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/102.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/103.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/104.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/105.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/106.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/107.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/108.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/109.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/110.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/111.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/112.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/113.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/114.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/115.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/116.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/117.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/118.jpg",
+    "https://raw.githubusercontent.com/immrakpk/cdn/main/119.jpg",
+];
+
+  // var sound = new Audio("https://www.freespecialeffects.co.uk/soundfx/animals/duck1.wav");
+  //var sound = new Audio("./.mp3");
+  var sound = new Audio(
+    "https://firebasestorage.googleapis.com/v0/b/test-35ebf.appspot.com/o/alarm.mp3?alt=media&token=139e9a1f-db85-4eb8-94eb-2ee93daaaa98"
+  );
+  sound.loop = true;
+  var h2 = document.getElementById("clock");
+  // display current time by the second
+  var currentTime = setInterval(function () {
+    var date = new Date();
+    var hours = 12 - date.getHours();
+    // var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var ampm = date.getHours() < 12 ? "AM" : "PM";
+    // convert military time to standard time
+    if (hours < 0) {
+      hours = hours * -1;
+    } else if (hours == 00) {
+      hours = 12;
+    } else {
+      hours = hours;
+    }
+    h2.textContent =
+      addZero(hours) +
+      ":" +
+      addZero(minutes) +
+      ":" +
+      addZero(seconds) +
+      "" +
+      ampm;
+  }, 1000);
+  function addZero(time) {
+    return time < 10 ? "0" + time : time;
+  }
+  function hoursMenu() {
+    var select = document.getElementById("alarmhrs");
+    var hrs = 12;
+    for (i = 1; i <= hrs; i++) {
+      select.options[select.options.length] = new Option(
+        i < 10 ? "0" + i : i,
+        i
+      );
+    }
+  }
+  hoursMenu();
+  function minMenu() {
+    var select = document.getElementById("alarmmins");
+    var min = 59;
+    for (i = 0; i <= min; i++) {
+      select.options[select.options.length] = new Option(
+        i < 10 ? "0" + i : i,
+        i
+      );
+    }
+  }
+  minMenu();
+  function secMenu() {
+    var select = document.getElementById("alarmsecs");
+    var sec = 59;
+    for (i = 0; i <= sec; i++) {
+      select.options[select.options.length] = new Option(
+        i < 10 ? "0" + i : i,
+        i
+      );
+    }
+  }
+  secMenu();
+
+  function alarmSet() {
+    var hr = document.getElementById("alarmhrs");
+    var min = document.getElementById("alarmmins");
+    var sec = document.getElementById("alarmsecs");
+    var ap = document.getElementById("ampm");
+
+    var selectedHour = hr.options[hr.selectedIndex].value;
+    var selectedMin = min.options[min.selectedIndex].value;
+    var selectedSec = sec.options[sec.selectedIndex].value;
+    var selectedAP = ap.options[ap.selectedIndex].value;
+
+    var alarmTime =
+      addZero(selectedHour) +
+      ":" +
+      addZero(selectedMin) +
+      ":" +
+      addZero(selectedSec) +
+      selectedAP;
+    console.log("alarmTime:" + alarmTime);
+
+    document.getElementById("alarmhrs").disabled = true;
+    document.getElementById("alarmmins").disabled = true;
+    document.getElementById("alarmsecs").disabled = true;
+    document.getElementById("ampm").disabled = true;
+
+    // when alarmtime is equal to currenttime then play a sound
+    var h2 = document.getElementById("clock");
+
+    /*function to calcutate the current time 
+                then compare it to the alarmtime and play a sound when they are equal
+                */
+
+    setInterval(function () {
+      var date = new Date();
+      var hours = 12 - date.getHours();
+      // var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var seconds = date.getSeconds();
+      var ampm = date.getHours() < 12 ? "AM" : "PM";
+
+      // convert military time to standard time
+
+      if (hours < 0) {
+        hours = hours * -1;
+      } else if (hours == 00) {
+        hours = 12;
+      } else {
+        hours = hours;
+      }
+
+      var currentTime = (h2.textContent =
+        addZero(hours) +
+        ":" +
+        addZero(minutes) +
+        ":" +
+        addZero(seconds) +
+        "" +
+        ampm);
+
+      if (alarmTime == currentTime) {
+        sound.play();
+      }
+    }, 1000);
+
+    // console.log('currentTime:' + currentTime);
+  }
+
+  function alarmClear() {
+    document.getElementById("alarmhrs").disabled = false;
+    document.getElementById("alarmmins").disabled = false;
+    document.getElementById("alarmsecs").disabled = false;
+    document.getElementById("ampm").disabled = false;
+    sound.pause();
+  }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    function formatTextDate(date_string, id) {
+        var d_names = [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+        ];
+    
+        var m_names = [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ];
+    
+        var dateArray = date_string.split("-"); // Need to convert to Array to get to work in all browsers. Example format: Year, month, day: 2013-06-18
+        var d = new Date(dateArray[0], dateArray[1] - 1, dateArray[2]); // Subtract 1 from the month because it is 0-11
+        var s_day = d.getDay();
+        var s_date = d.getDate();
+        var sup = ""; // To add superscript text to date.
+        var s_month = d.getMonth();
+        var s_year = d.getFullYear();
+    
+        // Following code is optional for adding a superscript to the day of the week
+    
+        if (s_date == 1 || s_date == 21 || s_date == 31) {
+          sup = "st";
+        } else if (s_date == 2 || s_date == 22) {
+          sup = "nd";
+        } else if (s_date == 3 || s_date == 23) {
+          sup = "rd";
+        } else {
+          sup = "th";
+        }
+    
+        // Example of "sup": document.write(d_names[s_day] + ", " + m_names[s_month] + " " + s_date + sup + ", " + s_year);
+    
+        var output_date =
+          d_names[s_day] + ", " + m_names[s_month] + " " + s_date + ", " + s_year;
+    
+        // console.log(output_date);
+    
+        // Pass the string to an element by ID or to a text input by ID
+        if (document.getElementById) {
+          x = document.getElementById(id);
+        } else if (document.all) {
+          // For IE Support
+          x = document.all[id];
+        }
+    
+        if (
+          x.tagName &&
+          x.tagName.toLowerCase() == "input" &&
+          x.type.toLowerCase() == "text"
+        ) {
+          // Check if this is a text input
+          console.log("this is a text input");
+          x.value = "";
+          x.value = output_date;
+        } else {
+          // Else this is an element
+          console.log("this is an element");
+          x.innerHTML = "";
+          x.innerHTML = output_date;
+        }
+      }
+    
+      var dateObj = new Date();
+      var month = dateObj.getUTCMonth() + 1; // months from 1-12
+      var day = dateObj.getUTCDate();
+      var year = dateObj.getUTCFullYear();
+    
+      formatTextDate(`${year}-${month}-${day}`, "display-date");
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    function ChangeIt() {
+        const random = Math.floor(Math.random() * img_list.length);
+        document.body.background = img_list[random];
+        document.body.style.backgroundSize = "cover"; // Background repeat
+      }
+      ChangeIt();
+});
